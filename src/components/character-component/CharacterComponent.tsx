d31d8c36ef37cd22004bce16035e4cb31d96162e
MartuhaKul <martysichkaaa15@gmail.com>
2025-01-06 20:17:14 +0200

    change hw-1 task-1

--- src/components/character-component/CharacterComponent.tsx
+++ src/components/character-component/CharacterComponent.tsx
@@ -9,9 +9,12 @@ interface CharacterComponentProps {
 
 export const CharacterComponent = ({item, children}: CharacterComponentProps) => {
     return (
-        <div className='font-mono my-3 border-yellow div-flex'>
+        <div className='font-mono border-yellow div-flex'>
            <h3 className='font-bold text-xl'>{item.name}{item.surname}</h3>
-            <p className='mt-5'>{children}</p>
+            <div className='wrapper-container'>
+                <p className='mt-5'>{children}</p>
+                <img src={item.photo} alt={item.name}/>
+            </div>
         </div>
     );
 };
