61ffb71adc333b31c7852c96cba782304c25a579
MartuhaKul <martysichkaaa15@gmail.com>
2025-01-07 19:29:10 +0200

    add hw-3 task-1

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(<App />)
+const routes = createBrowserRouter([
+    {}
+]);
+createRoot(document.getElementById('root')!)
+    .render(<RouterProvider router={routes} />)
