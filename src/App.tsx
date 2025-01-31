bf1dfdeb5261847b37eef2ba9fab07fe581252f6
MartuhaKul <martysichkaaa15@gmail.com>
2025-01-31 11:37:33 +0200

    done hw-7 task-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css'
+import {useFetch} from "./hooks/useFetch.tsx";
 
 function App() {
+    const posts = useFetch<{ id: number, title: string, body: string }[]>('https://jsonplaceholder.typicode.com/posts', []);
+    return (
+        <>
+            {
+                // posts &&
+                posts.map((post) => (
+                <div key={post.id} className="App">
+                    {post.title}{post.body}
 
-  return (
-    <></>
-  )
+                </div>
+            ))
+            }
+        </>
+    )
 }
 
 export default App
